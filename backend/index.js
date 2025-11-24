0be47abb151b72d6642c77857afe12d70ed57b0c
salman husainie <salmanhusainie@gmail.com>
2025-11-24 11:58:22 -0500

    jhmg vb

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,14 @@
 import express from "express";
-
 import routes from "./routes.js";
-
 import cors from "cors";
 
 const app = express();
-
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(cors({
     origin: FRONTEND_URL
 }));
 app.use(express.json());
-
 app.use('', routes);
-
-// Add a simple test route
-app.get('/health', (req, res) => {
-    res.send('OK');
-});
 
 export default app;
