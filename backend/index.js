e15a68a649f42a5387c319d8f35f040ec07a3dfa
salman husainie <salmanhusainie@gmail.com>
2025-11-24 11:10:37 -0500

    rwr

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,7 @@ import cors from "cors";
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-app.use(cors({
-    origin: FRONTEND_URL
-}));
+app.use(cors());
 app.use(express.json());
 app.use('', routes);
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 import app from "./index.js";
 
+console.log('Starting server...');
+
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
